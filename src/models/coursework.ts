d6d32e67dfa61adecc590b2e6eb3f2f8556e0575
Unknown <fafierrojr@gmail.com>
2018-05-27 18:39:59 -0700

    Refactored School Model- Added Coursework Model

---
+++ src/models/coursework.ts
@@ -0,0 +1,5 @@
+export class Coursework{
+    course_name: string
+    description: string
+    link: string
+}
--- src/models/school.ts
+++ src/models/school.ts
@@ -1,8 +1,13 @@
+import { Coursework } from "./coursework";
+
 export class School{
     name: string
     begin_year: number
     end_year: number
-    img_url: string;
+    img_url: string
+    honors: string
+    gpa: number
+    notable_coursework: {[key: string]: Coursework}
 
     /**
      * School Model
@@ -12,7 +17,8 @@ export class School{
      * @param end_year      the last year of attendance
      * @param img_url       the url of the school's card img stored in firestore
      */
-    constructor(name: string, begin_year: number, end_year: number, img_url: string){
+    
+    constructor(name: string, begin_year: number, end_year: number, img_url: string = ''){
         this.name = name
         this.begin_year = begin_year
         this.end_year = end_year
