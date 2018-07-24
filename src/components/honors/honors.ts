4758c11998075282027737203a54ccf94e06f6be
Francisco Fierro <fafierrojr@gmail.com>
2018-07-24 15:51:14 -0700

    created honors component

---
+++ src/components/honors/honors.ts
@@ -0,0 +1,28 @@
+import { Component, Input } from '@angular/core';
+import { EfSchoolsProvider } from '../../providers/efschoolsprovider/efschoolsprovider';
+import { Observable } from 'rxjs/Observable';
+
+/**
+ * Generated class for the HonorsComponent component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: 'honors',
+  templateUrl: 'honors.html'
+})
+export class HonorsComponent {
+
+  honors: Observable<string[]>
+  @Input() school_key: string
+
+  constructor(public schoolsProvider: EfSchoolsProvider) {
+    console.log('Hello HonorsComponent Component');
+    this.honors = schoolsProvider.getSchoolProperty(this.school_key, 'Honors') as Observable<string[]>;
+
+  }
+
+  
+
+}
