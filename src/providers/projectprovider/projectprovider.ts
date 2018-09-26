d73daf6cb636eabec6f25bb50e6b1534b6b79c64
Francisco Fierro <fafierrojr@gmail.com>
2018-09-26 14:39:56 -0700

    added course an project providers

---
+++ src/providers/projectprovider/projectprovider.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Project } from '../../models/project';
+
+/*
+  Generated class for the ProjectProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class ProjectProvider {
+  route = "https://api.franciscofierro.us/api/projects"
+  //route = "http://localhost:5000/api/schools"
+
+
+  constructor(public http: HttpClient) {
+    console.log('Hello ProjectProvider Provider');
+  }
+
+  getAllProjects(): Observable<Project[]>{
+    return this.http.get<Project[]>(this.route)
+  }
+  
+  getProjectsForCourse(courseName: string): Observable<Project[]>{
+    return this.http.get<Project[]>(this.route + '/' + courseName);
+  }
+
+  getProject(projectKey: string): Observable<Project>{
+    return this.http.get<Project>(this.route + '/' + projectKey)
+  }
+
+}
